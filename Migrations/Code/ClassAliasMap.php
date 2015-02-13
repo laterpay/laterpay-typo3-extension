86f092b53e8461ab9231c115e35e9999db329491
oleggnl <ohainulin@scnsoft.com>
2015-02-13 16:51:00 +0300

    Initial commit for skeleton of laterpay typo3 extention.

---
+++ Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	'Tx_Laterpay_Controller_LaterpayController' => 'TYPO3\\CMS\\Laterpay\\Controller\\LaterpayController',
+	'Tx_Laterpay_Domain_Model_Extension' => 'TYPO3\\CMS\\Laterpay\\Domain\\Model\\Extension',
+	'Tx_Laterpay_Domain_Repository_ExtensionRepository' => 'TYPO3\\CMS\\Laterpay\\Domain\\Repository\\ExtensionRepository',
+	'Tx_Laterpay_ViewHelpers_SkinImageViewHelper' => 'TYPO3\\CMS\\Laterpay\\ViewHelpers\\SkinImageViewHelper',
+);
---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "we_google_analytics".
+ *
+ * Auto generated 04-02-2015 12:22
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'LaterPay',
+	'description' => 'LaterPay plugin.',
+	'category' => 'fe',
+	'version' => '0.0.1',
+	'state' => 'alpha',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'admin@laterpay.com',
+	'author_email' => 'admin@laterpay.com',
+	'author_company' => 'Äôåóêçôí AG',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '4.3.0-6.2.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
